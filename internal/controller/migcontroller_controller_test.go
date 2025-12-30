4feb3957c837edcf53b528cc5399b0dc62d4ed34
Alex Kalenyuk <akalenyu@redhat.com>
2025-12-30 17:15:02 +0200

    fix unit tests to not start controller/ensure clean env
    second test addition exposed an issue with env clean up;
    since we use envtest we actually need to clean stuff up
    from a somewhat "real" apiserver/etcd! cool stuff
    
    Signed-off-by: Alex Kalenyuk <akalenyu@redhat.com>

--- internal/controller/migcontroller_controller_test.go
+++ internal/controller/migcontroller_controller_test.go
@@ -18,16 +18,24 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"reflect"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	record "k8s.io/client-go/tools/record"
-	manager "sigs.k8s.io/controller-runtime/pkg/manager"
 
 	"kubevirt.io/controller-lifecycle-operator-sdk/pkg/sdk/callbacks"
 	sdkr "kubevirt.io/controller-lifecycle-operator-sdk/pkg/sdk/reconciler"
@@ -53,6 +61,7 @@ var _ = Describe("MigController Controller", func() {
 			Namespace: testNamespace,
 		}
 		migcontroller := &migrationsv1alpha1.MigController{}
+		defaultResult := reconcile.Result{RequeueAfter: requeueInterval}
 
 		BeforeEach(func() {
 			By("creating the custom resource for the Kind MigController")
@@ -70,8 +79,9 @@ var _ = Describe("MigController Controller", func() {
 			Expect(k8sClient.Status().Update(ctx, migcontroller)).To(Succeed())
 
 			controllerReconciler = &MigControllerReconciler{
-				Client: k8sClient,
-				scheme: k8sClient.Scheme(),
+				namespace: testNamespace,
+				Client:    k8sClient,
+				scheme:    k8sClient.Scheme(),
 				namespacedArgs: &namespaced.FactoryArgs{
 					OperatorVersion: "0.0.1",
 					Namespace:       testNamespace,
@@ -84,18 +94,20 @@ var _ = Describe("MigController Controller", func() {
 				},
 			}
 
-			mgr, err := manager.New(cfg, manager.Options{
-				Scheme: k8sClient.Scheme(),
-			})
-			Expect(err).NotTo(HaveOccurred())
 			callbackDispatcher := callbacks.NewCallbackDispatcher(log, k8sClient, k8sClient, k8sClient.Scheme(), testNamespace)
+			noOpGetCache := func() cache.Cache {
+				// no watching, so no cache
+				return nil
+			}
 			controllerReconciler.reconciler = sdkr.NewReconciler(
 				controllerReconciler, log, k8sClient,
-				callbackDispatcher, k8sClient.Scheme(), mgr.GetCache,
+				callbackDispatcher, k8sClient.Scheme(), noOpGetCache,
 				createVersionLabel, updateVersionLabel, LastAppliedConfigAnnotation,
 				requeueInterval, finalizerName, true, recorder,
-			).WithNamespacedCR()
-			Expect(controllerReconciler.SetupWithManager(mgr)).To(Succeed())
+			).
+				WithNamespacedCR().
+				WithWatching(true)
+			controllerReconciler.registerHooks()
 		})
 
 		AfterEach(func() {
@@ -106,6 +118,23 @@ var _ = Describe("MigController Controller", func() {
 
 			By("Cleanup the specific resource instance MigController")
 			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+			By("Reconciling the deletion")
+			res, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(res).To(Equal(reconcile.Result{}))
+			err = k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).To(MatchError(k8serrors.IsNotFound, "k8serrors.IsNotFound"))
+			// no garbage collection with envtest, so we need to delete resources manually
+			deleteOwnedResources(ctx, k8sClient, resource)
+			// Wait for all CRDs to be deleted which is apparently slow on envtest
+			Eventually(func(g Gomega) []apiextensionsv1.CustomResourceDefinition {
+				crdList := &apiextensionsv1.CustomResourceDefinitionList{}
+				err := k8sClient.List(ctx, crdList)
+				g.Expect(err).ToNot(HaveOccurred())
+				return crdList.Items
+			}, time.Second*5, time.Second*1).Should(HaveLen(1), "All CRDs other than the one for MigController should be deleted")
 		})
 
 		It("should successfully reconcile the resource", func() {
@@ -115,10 +144,11 @@ var _ = Describe("MigController Controller", func() {
 
 			Expect(resource.Status.ObservedVersion).To(Equal("0.0.1"))
 			By("Reconciling the created resource")
-			_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+			res, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
 				NamespacedName: typeNamespacedName,
 			})
 			Expect(err).NotTo(HaveOccurred())
+			Expect(res).To(Equal(defaultResult))
 			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
 			// Example: If you expect a certain status condition after reconciliation, verify it here.
 		})
@@ -130,10 +160,11 @@ var _ = Describe("MigController Controller", func() {
 
 			Expect(resource.Status.ObservedVersion).To(Equal("0.0.1"))
 			By("Reconciling the created resource")
-			_, err = controllerReconciler.Reconcile(ctx, reconcile.Request{
+			res, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
 				NamespacedName: typeNamespacedName,
 			})
 			Expect(err).NotTo(HaveOccurred())
+			Expect(res).To(Equal(defaultResult))
 
 			clusterRole := &rbacv1.ClusterRole{}
 			err = k8sClient.Get(ctx, types.NamespacedName{Name: "migrations.kubevirt.io:view"}, clusterRole)
@@ -161,3 +192,47 @@ var _ = Describe("MigController Controller", func() {
 		})
 	})
 })
+
+// deleteOwnedResources deletes all resources owned by the given owner resource
+func deleteOwnedResources(ctx context.Context, k8sClient client.Client, owner client.Object) {
+	// Get the owner's UID for comparison
+	ownerUID := owner.GetUID()
+	ownerNamespace := owner.GetNamespace()
+
+	// List of resource types that might be owned by the CR
+	resourceTypes := []client.ObjectList{
+		&corev1.ConfigMapList{},
+		&corev1.SecretList{},
+		&corev1.ServiceList{},
+		&corev1.ServiceAccountList{},
+		&rbacv1.RoleList{},
+		&rbacv1.RoleBindingList{},
+		&appsv1.DeploymentList{},
+		// Add more resource types as needed
+	}
+
+	for _, resourceList := range resourceTypes {
+		// List all resources of this type in the namespace
+		Expect(k8sClient.List(ctx, resourceList, client.InNamespace(ownerNamespace))).To(Succeed())
+
+		// Use reflection to access the Items field
+		items := reflect.ValueOf(resourceList).Elem().FieldByName("Items")
+		if !items.IsValid() {
+			continue
+		}
+
+		// Iterate through items and delete those owned by our CR
+		for i := 0; i < items.Len(); i++ {
+			item := items.Index(i).Addr().Interface().(client.Object)
+
+			// Check if this resource is owned by our CR
+			for _, ownerRef := range item.GetOwnerReferences() {
+				if ownerRef.UID == ownerUID {
+					By(fmt.Sprintf("Deleting owned %T: %s", item, item.GetName()))
+					Expect(k8sClient.Delete(ctx, item)).To(Succeed())
+					break
+				}
+			}
+		}
+	}
+}
